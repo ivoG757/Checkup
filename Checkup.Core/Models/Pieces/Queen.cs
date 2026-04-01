82869c95e476f1ae5da7ce590fd8a25e508183ab
ivoge <ivoge@IVOSPC>
2026-04-02 00:10:06 +0300

    Add Queen piece logic and enable Queen placement on board
    Implemented the Queen class with full move logic, combining rook and bishop movement patterns. Updated Rook class parameter names for clarity. Enabled placement of both black and white Queens during board initialization in ChessEngineService.

--- Checkup.Core/Models/Pieces/Queen.cs
+++ Checkup.Core/Models/Pieces/Queen.cs
@@ -5,13 +5,14 @@ using System.Text;
 
 namespace Checkup.Core.Models.Pieces
 {
-    public class Rook : BasePiece
+    public class Queen : BasePiece
     {
-        public override bool IsBlack { get; set; } = false;
-        public override PieceType Type => PieceType.Rook;
+        public override bool IsBlack { get; set; } = true;
+        public override PieceType Type => PieceType.Queen;
+
         public override List<(int x, int y)> GetValidMoves(Board board, int currentX, int currentY)
         {
-            var validMoves = new List<(int, int)>();
+            var validMoves = new List<(int x, int y)>();
 
             // Up
             for (int i = currentX - 1; i >= 0; i--)
@@ -65,6 +66,37 @@ namespace Checkup.Core.Models.Pieces
                 }
             }
 
+            validMoves.AddRange(CheckDirection(board, currentX, currentY, 1, 1));   // Bottom-right diagonal
+            validMoves.AddRange(CheckDirection(board, currentX, currentY, 1, -1));  // Top-right diagonal
+            validMoves.AddRange(CheckDirection(board, currentX, currentY, -1, 1));  // Bottom-left diagonal
+            validMoves.AddRange(CheckDirection(board, currentX, currentY, -1, -1)); // Top-left diagonal
+
+            return validMoves;
+        }
+        private List<(int, int)> CheckDirection(Board board, int currentX, int currentY, int dx, int dy)
+        {
+            var validMoves = new List<(int x, int y)>();
+
+            int j = currentY + dy;
+            int x = currentX + dx;
+
+            while (IsInBounds(x, j))
+            {
+                if (board.Squares[x, j] == null)
+                {
+                    validMoves.Add((x, j));
+                }
+                else
+                {
+                    if (board.Squares[x, j].IsBlack != this.IsBlack)
+                    {
+                        validMoves.Add((x, j)); // Can capture opponent's piece
+                    }
+                    break; // Stop checking in this direction after hitting a piece
+                }
+                x += dx;
+                j += dy;
+            }
             return validMoves;
         }
     }
--- Checkup.Core/Services/ChessEngineService.cs
+++ Checkup.Core/Services/ChessEngineService.cs
@@ -17,7 +17,7 @@ namespace Checkup.Core.Services
             PlacePiece(0, 0, new Rook { IsBlack = true });
             PlacePiece(0, 1, new Knight { IsBlack = true });
             PlacePiece(0, 2, new Bishop { IsBlack = true });
-            //PlacePiece(0, 3, new Queen { IsBlack = true });
+            PlacePiece(0, 3, new Queen { IsBlack = true });
             //PlacePiece(0, 4, new King { IsBlack = true });
             PlacePiece(0, 5, new Bishop { IsBlack = true });
             PlacePiece(0, 6, new Knight { IsBlack = true });
@@ -35,7 +35,7 @@ namespace Checkup.Core.Services
             PlacePiece(7, 0, new Rook { IsBlack = false });
             PlacePiece(7, 1, new Knight { IsBlack = false });
             PlacePiece(7, 2, new Bishop { IsBlack = false });
-            //PlacePiece(7, 3, new Queen { IsBlack = false });
+            PlacePiece(7, 3, new Queen { IsBlack = false });
             //PlacePiece(7, 4, new King { IsBlack = false });
             PlacePiece(7, 5, new Bishop { IsBlack = false });
             PlacePiece(7, 6, new Knight { IsBlack = false });
